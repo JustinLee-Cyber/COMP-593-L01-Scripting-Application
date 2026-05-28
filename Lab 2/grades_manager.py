d53b933ae070bf1c13932a1547e6c44191b744b1
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-05-28 15:46:06 -0400

    Update of display
    Added a second function of displaying student grades, it is another way of doing it.

--- Lab 2/grades_manager.py
+++ Lab 2/grades_manager.py
@@ -10,33 +10,92 @@ def calculate_average(grades): #creating a function to get the average grades th
 
     return average #return, must always include this in a function for this case, return the average number
 
-def main(): #main function
+def display(object): #creating another function
+    for _ in object: #for loop
+        print(_) #print _
 
-    students = ["Ali", "sara", "John", "david"] #A list array that holds onto variables which is in this case is a string
-    grades = [85,92,78,90] #A list of array that holds onto variables which is currently int variables
+    return #return
 
-    #print(students) #to print in the consule of what is in the variable students list array
-    #print(grades) #to print in the consule of what is in the variable grades list array
+def passingGrades(Students, Grades): #function that takes in name list of student and grades list
+    #Creating variables
+    counter = 0;
+    Passed = 0;
+    Failed = 0;
 
-    course_info = ("Scripting Applications", "comp593", 2) #this is a tuples that cannot be changed and is called course_info
+    #For loop where it grabs a name from the student list and checks
+    for Name in Students:  
+        #If statement where it checks if the passing grade is over 80 or not in Grades list with its index than prints
+        if Grades[counter] >= 80:
+            print(f"Print current index: {counter}")
+            print (f"Student {Name} has a passing grade of {Grades[counter]}")
+            Passed +=1;
+        #Else after the if statement if it faileds the over 80 in the Grades
+        else:
+            print(f"Print current index: {counter}")
+            print (f"Student {Name} has not passed with a grade of {Grades[counter]}")
+            Failed +=1;
+        counter +=1;
+    
+    #Print how many have passed and how many failed
+    print(f"Passed: {Passed} \nFailed: {Failed}")
+    
+    return #return to main
 
-    #print("This is the course information") #create another print to explain what is below
-    #print(course_info) #To print in the consule of what is in the variable course_info and this is a tuples
+def passingGrades2(Students, Grades):
+    
+    #Creating variables for students who passed or failed
+    Passed = 0;
+    Failed = 0;
+    
+    #Creating a range for loop using range to find the index and if statement
+    for i in range(len(Students)):
 
-    print(f"This is the course information : {course_info}") #using f string to send out text and string from variable
+        if Grades[i] >= 80:
+            #print(f"Print current index: {i}")
+            print (f"Student {Students[i]} has a passing grade of {Grades[i]}")
+            Passed +=1;
+        #Else after the if statement if it faileds the over 80 in the Grades
+        else:
+            #print(f"Print current index: {i}")
+            print (f"Student {Students[i]} has not passed with a grade of {Grades[i]}")
+            Failed +=1;
+    
+    #Print how many have passed and how many failed
+    print(f"Passed: {Passed} \nFailed: {Failed}")
 
-    for student in students: #creating a for loop for students list
-        { #starting bracket
-            print(f"Name: {student.capitalize()}") #print the student list one by one
-        } #ending bracket
+    #return to main
+    return 
 
+def main(): #main function 
+
+    #Creating a list for name of Students and their Grades - A list can be adjusted or changed
+    students = ["Ali", "sara", "John", "david"] 
+    grades = [85,92,78,90] 
+
+     #this is a tuples that cannot be changed and is called course_info
+    course_info = ("Scripting Applications", "comp593", 2)
+
+    #using f string to send out text and string from variable
+    print(f"This is the course information : {course_info}") 
+
+    #Calling the function of display to showcase the student names
+    display(students)
+
+    #Getting the average number using the function calculate_average
     average_grade = calculate_average(grades);
 
+    #print the average base on the variabe saved and given from the function calculate_average
     print(f"The average grades in the class is: {average_grade}.")
 
+    #Testing the function create of passingGrades
+    #print("Test of function passingGrades")
+    #passingGrades(students, grades);
+
+    #Testing the function create of passingGrades2
+    #print("Test function of passingGrades2")
+    passingGrades2(students, grades)
+
     return
-
-
 
 if __name__ == '__main__': #very important when doing the main
     main()
