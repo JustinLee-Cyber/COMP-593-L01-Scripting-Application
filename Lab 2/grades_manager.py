0e92f6295f2dcc038caa45360b2ed6821261dc47
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-05-28 14:14:47 -0400

    Creating and adding lab 2 for COMP 593 Scripting Application Lab
    This is the creation of the folders to hold onto all labs for scripting applications term 2, first one is lab 1.

---
+++ Lab 2/grades_manager.py
@@ -0,0 +1,23 @@
+
+
+
+
+def main():
+
+    students = ["Ali", "sara", "John", "david"] #A list array that holds onto variables which is in this case is a string
+    grades = [85,92,78,90] #A list of array that holds onto variables which is currently int variables
+
+    print(students) #to print in the consule of what is in the variable students list array
+    print(grades) #to print in the consule of what is in the variable grades list array
+
+
+
+
+
+
+    return
+
+
+
+if __name__ == '__main__':
+    main()
