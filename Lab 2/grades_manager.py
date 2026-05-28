25f31bb213ae75113ebec816c4682e8ed118ef07
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-05-28 14:29:15 -0400

    Update grades_manager.py
    Adding for loop plus adjustments

--- Lab 2/grades_manager.py
+++ Lab 2/grades_manager.py
@@ -7,16 +7,20 @@ def main():
     students = ["Ali", "sara", "John", "david"] #A list array that holds onto variables which is in this case is a string
     grades = [85,92,78,90] #A list of array that holds onto variables which is currently int variables
 
-    print(students) #to print in the consule of what is in the variable students list array
-    print(grades) #to print in the consule of what is in the variable grades list array
+    #print(students) #to print in the consule of what is in the variable students list array
+    #print(grades) #to print in the consule of what is in the variable grades list array
 
     course_info = ("Scripting Applications", "comp593", 2) #this is a tuples that cannot be changed and is called course_info
 
-    print("This is the course information") #create another print to explain what is below
-    print(course_info) #To print in the consule of what is in the variable course_info and this is a tuples
+    #print("This is the course information") #create another print to explain what is below
+    #print(course_info) #To print in the consule of what is in the variable course_info and this is a tuples
 
+    print(f"This is the course information : {course_info}") #using f string to send out text and string from variable
 
-
+    for students in students: #creating a for loop for students list
+        { #starting bracket
+            print(f"Name: {students.capitalize}") #print the student list one by one
+        } #ending bracket
 
 
     return
