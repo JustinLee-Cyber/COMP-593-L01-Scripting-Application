c458b8e3ef7a872e378eeb6941e44bac6278647b
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-05-28 14:37:39 -0400

    Update grades_manager.py
    Adding the new function and creating more print statement using the f string

--- Lab 2/grades_manager.py
+++ Lab 2/grades_manager.py
@@ -1,8 +1,16 @@
 
 
+def calculate_average(grades): #creating a function to get the average grades that is being provided
 
+    total = 0; #creating a placeholder variable to use to get the total
+    for grade in grades: #creating a for loop to go and use every grade in the list that was provided
+        total +=grade #adding the current grade variable to the total and get the equal of it
 
-def main():
+    average = total / len(grades) #grabing the total variable and divide by length of grades list array (which has int variables inside)
+
+    return average #return, must always include this in a function for this case, return the average number
+
+def main(): #main function
 
     students = ["Ali", "sara", "John", "david"] #A list array that holds onto variables which is in this case is a string
     grades = [85,92,78,90] #A list of array that holds onto variables which is currently int variables
@@ -17,15 +25,18 @@ def main():
 
     print(f"This is the course information : {course_info}") #using f string to send out text and string from variable
 
-    for students in students: #creating a for loop for students list
+    for student in students: #creating a for loop for students list
         { #starting bracket
-            print(f"Name: {students.capitalize}") #print the student list one by one
+            print(f"Name: {student.capitalize()}") #print the student list one by one
         } #ending bracket
 
+    average_grade = calculate_average(grades);
+
+    print(f"The average grades in the class is: {average_grade}.")
 
     return
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #very important when doing the main
     main()
