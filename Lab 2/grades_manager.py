a53dcf44aa38a9fdafac7ce16827aced41c0bf26
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-05-28 14:20:19 -0400

    Update grades_manager.py
    Updated of the code

--- Lab 2/grades_manager.py
+++ Lab 2/grades_manager.py
@@ -10,6 +10,10 @@ def main():
     print(students) #to print in the consule of what is in the variable students list array
     print(grades) #to print in the consule of what is in the variable grades list array
 
+    course_info = ("Scripting Applications", "comp593", 2) #this is a tuples that cannot be changed and is called course_info
+
+    print("This is the course information") #create another print to explain what is below
+    print(course_info) #To print in the consule of what is in the variable course_info and this is a tuples
 
 
 
