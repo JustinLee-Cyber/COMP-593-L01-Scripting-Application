dcd0de21dbaecc8beb720d7167a3415b5310380d
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-06-04 14:18:41 -0400

    Create FileWorldCup2026Demo.py
    Added demo code to learn about dictionary this is not the submission for lab 3

---
+++ Lab 3/FileWorldCup2026Demo.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+def main():
+
+	# Main program logic
+	#Fifa Dictionary 
+	fifa_data = {
+		'name':'Lionel Messi', #player name
+		'playernumber':'10', #plyer number
+		'stadiums':['Toronto','New York','Azteco'], #location in list
+		'matches':[ #added dictionary list
+			{'team':'Protogal vs Argintena', 'stage':'Finals'}, #information is teams who are facing and stage - semi-finals or finals
+			{'team':'Sinegal vs Holand', 'stage':'Semi-Finals'}
+		]
+	}
+
+	return
+
+if __name__ == '__main__':
+	main()
