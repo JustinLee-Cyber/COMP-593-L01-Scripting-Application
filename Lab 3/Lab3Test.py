ad07fcccc4f85f22870fb9da962dfebf295a2c10
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-06-05 18:24:06 -0400

    Final finished code for lab 3
    Update to the code and it is finished

--- Lab 3/Lab3Test.py
+++ Lab 3/Lab3Test.py
@@ -1,11 +1,11 @@
 #Student Name: Justin Lee
 #Student Number: 10152040
 #Lab 3
+#Step 1 - start writing the scripts :D
 
 # Create functions
-
-#Step 3:
-#function to print name - grab information from created dictinary to print, split and show ID
+# Step 3:
+# Function to print name - grab information from created dictinary to print, split and show ID
 def printName(data):
 	
       Fullname = data['name']
@@ -15,23 +15,63 @@ def printName(data):
 
       return
 
-#Step 4:
-#function to print pizza toppings that Justin like!
+# Step 4:
+# Function to print pizza toppings that Justin like!
 def printtoppings(data):
 	
       print("My favourite pizza toppings are:")
 
       for _ in data['pizza_toppings']:
-          print(_)
+          print(f'- {_}')
 
       return
 
-#Step 5:
+# Step 5:
+# Function: Added extra likeable toppings using a tuple into the data key 'pizza_toppings' list
 def addtoppings(data,toppings):
       
       data['pizza_toppings'].extend(toppings)
 
       data['pizza_toppings'].sort()
+
+      data['pizza_toppings'] = list(map(str.lower, data['pizza_toppings']))
+
+      return
+
+# Step 6: 
+# Function: Added movies from title and genre string - from main
+def addmovies(data, title, genre):
+
+      Add_NewMovie = {'title':title, 'genre':genre}
+
+      data['movies'].append(Add_NewMovie)
+
+      return
+
+# Step 7:
+# Function: Printed the data key movies but the genre only
+def printgenre(data):
+      
+      print("I like to watch", end=" ")
+
+      for i in range(len(data['movies'])):
+            if i == len(data['movies']) - 1:
+                  print(f"and {data['movies'][i]['genre']} movies.")
+            else:  
+                  print(f"{data['movies'][i]['genre']}", end=", ")
+      return
+
+# Step 8: 
+# Function: printed movie titles from the data key title
+def printtitle(data):
+
+      print ("Some of my favourite movies are", end=" ")
+
+      for i in range(len(data['movies'])):
+            if i == len(data['movies']) - 1:
+                  print(f'and {data['movies'][i]['title'].title()}!')
+            else:
+                  print(f'{data['movies'][i]['title'].title()}', end=', ')
 
       return
 
@@ -42,19 +82,34 @@ def main():
       information = {
             'name':'Justin Lee',
             'student_ID': '10152040',
-            'pizza_toppings':['Cheese', 'Bacon', 'Pepperoni'],
+            'pizza_toppings':['CHEESE', 'BACON', 'PEPPERONI'],
             'movies':[
                   {'title':'fast & furious', 'genre':'action'},
-                  {'title':'the batman', 'genre':'crime/mystery'}
+                  {'title':'thor', 'genre':'fantasy'}
             ]
       }
 
-      printName(information)
-      printtoppings(information)
+      #Calling the functions in order to create the results using the functions
+      print("Lab 3 print results:")
 
-      addtoppings(information, ('Pineapple', 'sausesage', 'extra_cheese'))
-      printtoppings(information)
+      printName(information) #Step 3 print
+      print('------------------') 
+
+      printtoppings(information)#Step 4 print
+      print('------------------') 
+
+      addtoppings(information, ('Pineapple', 'sausesage', 'extra_cheese')) #Step 5 Add new toppings
+      printtoppings(information)#Step 4 with adjustments from Step 5
+      print('------------------')
+
+      addmovies(information, 'spider-man 2', 'romance')
+      printgenre(information) #Step 7 print
+      print('------------------') 
+
+      printtitle(information)#Step 8 print
+      print('------------------') 
 
       return
+
 if __name__ == '__main__':
 	main()
