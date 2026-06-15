0c815b6948351dc0a31e7c2ebb9b1b4161248d56
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-06-14 22:36:05 -0400

    Update

---
+++ Lab 4/Lab4_Code/Lab4_Script.py
@@ -0,0 +1,92 @@
+# pandas package
+import pandas as pd
+# os package 
+import os
+# sys package
+import sys
+# datetime package
+import datetime
+# re package
+import re
+
+
+"""
+Description:
+    Divides Conference Workshop data CSV file into divisional report Excel files.
+"""
+def process_order_data(data_csv, workshops_path):
+    
+    #Import order data csv that was inserted from command line
+    df = pd.read_csv(data_csv)
+
+    #REQ-6 - Drop unneeded columns
+    df.drop(columns=['ORDER ID', 'ADDRESS', 'CITY', 'STATE', 'POSTAL CODE', 'COUNTRY'], inplace=True)
+    #Wprint(df)
+
+    #REQ-7 - Sort by Item number
+    df.sort_values('ITEM NUMBER', ascending=True, inplace=True)
+
+    #REQ-8 - Insert total codes from item quantity x item price
+    df.insert(6, 'TOTAL PRICE', df['ITEM QUANTITY'] * df['ITEM PRICE'])
+    #print(df)
+
+    #REQ-9 - The grand sum of the total cost 
+    for order_date,  order_date_df in df.groupby('ORDER DATE'):
+        #print(order_date)
+        #print(order_date_df)
+        total_cost_sum = order_date_df['TOTAL PRICE'].sum()
+    #print(total_cost_data)
+        total_cost_df = pd.DataFrame({'ITEM PRICE':'GRAND TOTAL', 'TOTAL PRICE':[total_cost_sum]})
+    #print(total_cost_df)
+        order_date_df = pd.concat([order_date_df, total_cost_df])
+        #print(order_date_df)
+
+        fix_date = str(order_date).replace('/', '-')
+        file_name = f'ORDER_{fix_date}.xlsx' # 
+        data_file_path = os.path.join(workshops_path,file_name)
+        #print(data_file_path)
+    #REQ-10 - format time, adjust all price information as money, $, comma-spearated thousands, and two decimal places
+
+        with pd.ExcelWriter(data_file_path, engine="xlsxwriter") as writer:
+            order_date_df.to_excel(writer, index=False, sheet_name="Sheet1")
+
+        #workbook = writer.book
+        #worksheet = writer.sheets
+
+    return
+
+def main():
+    # Paths for input data and output directory
+
+    #REQ-1 - Grab CSV file data from command line parameter
+    from sys import argv
+
+    #REQ-2 If no command line parameter is provided, provide error message than exit
+    if len(argv) < 2:
+        print('Error: No path was input, please include path to CSV file.')
+        sys.exit()   
+    #REQ-3 If path was found, check if CSV is called for, if not error message and exit
+    else:    
+        data_csv = argv[1]
+        if not data_csv.endswith('.csv'):
+            print('Error: Path given does not include .csv.')
+            sys.exit()
+        else:
+            #Check if there is a created Orders Folder or Not
+            order_path = './Orders'
+            os.makedirs(order_path, exist_ok=True)
+
+            #Grab current date and time
+            #today_date = datetime.date.today()
+
+            #Create name for new folder per time use
+            #normal_path = '/Orders_' + str(today_date) 
+            #data_path = order_path + normal_path
+            #os.makedirs(data_path, exist_ok=True)
+            # Call the processing function
+            process_order_data(data_csv, order_path)
+
+    return
+
+if __name__ == '__main__':
+    main()
