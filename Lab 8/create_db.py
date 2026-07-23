bde1a48bcbbad9abf7e93c6a7d19adf6803d4376
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-07-23 15:33:45 -0400

    Update
    Added code and working code for lab 8

--- Lab 8/create_db.py
+++ Lab 8/create_db.py
@@ -7,6 +7,10 @@ Usage:
  python create_db.py
 """
 import os
+import sqlite3
+from datetime import datetime
+from faker import Faker
+
 
 # Determine the path of the database
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -18,15 +22,81 @@ def main():
 
 def create_people_table():
     """Creates the people table in the database"""
+
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+
     # TODO: Create function body
     # Hint: See example code in lab instructions entitled "Creating a Table"
+
+    
+    ppl_table_query = """
+        CREATE TABLE IF NOT EXISTS people
+        (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            address TEXT NOT NULL,
+            city TEXT NOT NULL,
+            province TEXT NOT NULL,
+            bio TEXT,
+            age INTEGER,
+            created_at DATETIME NOT NULL,
+            updated_at DATETIME NOT NULL 
+        );
+"""
+    #Execute the SQL query to create the 'people' table
+    cur.execute(ppl_table_query)
+
+    con.commit()
+    con.close()
+
     return
 
 def populate_people_table():
     """Populates the people table with 200 fake people"""
+
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+
     # TODO: Create function body
     # Hint: See example code in lab instructions entitled "Inserting Data into a Table"
     # Hint: See example code in lab instructions entitled "Working with Faker"
+
+    add_person_query = """
+    INSERT INTO people
+    (
+        name,
+        email,
+        address,
+        city,
+        province,
+        bio,
+        age,
+        created_at,
+        updated_at 
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+"""
+    fake = Faker("en_CA")
+    #define and create the tuple of data for the new person to be insert into the people table
+    for _ in range(200):
+        #tuble of data that will be insert - fake person
+        new_person = (fake.name(),
+                      fake.ascii_email(),
+                      fake.street_address(),
+                      fake.city(),
+                      fake.administrative_unit(),
+                      fake.sentence(nb_words=5),
+                      fake.random_int(min=1, max=100),
+                      datetime.now(),
+                      datetime.now())
+        #Add the fake person to the data base
+        cur.execute(add_person_query,new_person)
+
+    con.commit()
+    con.close()
+
     return
 
 if __name__ == '__main__':
--- Lab 8/old_people.py
+++ Lab 8/old_people.py
@@ -6,8 +6,10 @@ Description:
 Usage:
  python old_people.py
 """
+import sqlite3
 import os
 from create_db import db_path, script_dir
+import pandas as pd
 
 def main():
     old_people_list = get_old_people()
@@ -24,7 +26,22 @@ def get_old_people():
     """
     # TODO: Create function body
     # Hint: See example code in lab instructions entitled "Getting People Data from the Database"
-    return
+    
+    #connect to servert
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+
+    cur.execute('SELECT name, age FROM people WHERE age >= 50')
+
+    all_people = cur.fetchall()
+
+    #print(all_people)
+
+    con.commit()
+    con.close()
+
+    return all_people
+
 
 def print_name_and_age(name_and_age_list):
     """Prints name and age of all people in provided list
@@ -34,6 +51,10 @@ def print_name_and_age(name_and_age_list):
     """
     # TODO: Create function body
     # Hint: Use a for loop to iterate the list of tuples to print a sentence for each old person
+    
+    for i in name_and_age_list:
+        print(f"{i[0]} is {i[1]} years old")
+
     return
 
 def save_name_and_age_to_csv(name_and_age_list, csv_path):
@@ -45,6 +66,17 @@ def save_name_and_age_to_csv(name_and_age_list, csv_path):
     """
     # TODO: Create function body
     # Hint: In Lab 3, we converted a list of tuples into a pandas DataFrame and saved it to a CSV file
+
+    df = pd.DataFrame(name_and_age_list)
+    csv_filename = "Old_people_name_&_age.csv"
+    headings = ('Name', 'Age')
+    fixdirector=os.path.dirname(csv_path)
+    list_path=os.path.join(fixdirector, csv_filename)
+    #print(list_path)
+    df.to_csv(list_path, index=False, header=headings)
+
+    print(list_path)
+
     return
 
 if __name__ == '__main__':
