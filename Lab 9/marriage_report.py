6770aa26c698a0af240c313a8a6f3e3b1bbcf9b2
Justin Lee <Justin.Lee@flemingcollege.ca>
2026-07-30 15:14:29 -0400

    Update

--- Lab 9/marriage_report.py
+++ Lab 9/marriage_report.py
@@ -10,6 +10,8 @@ import os
 import sqlite3
 from create_relationships import db_path, script_dir
 
+import pandas as pd
+
 def main():
     # Query DB for list of married couples
     married_couples = get_married_couples()
@@ -27,7 +29,27 @@ def get_married_couples():
     # TODO: Function body
     # Hint: See example code in lab instructions entitled "Get a List of Relationships"
     con = sqlite3.connect(db_path)
-    return
+    cur = con.cursor()
+
+    # SQL query to get all relationships
+    #Test SELECT person1.name, person2.name, start_date, type FROM relationships to test output
+    all_relationships_query = """
+        SELECT person1.name, person2.name, start_date FROM relationships
+        JOIN people person1 ON person1_id = person1.id
+        JOIN people person2 ON person2_id = person2.id
+        WHERE type = 'spouse';
+"""
+
+    cur.execute(all_relationships_query)
+    
+    spouse_relationships = cur.fetchall()
+    con.close()
+
+    # Print sentences describing each relationship - testing
+    #for person1, person2, start_date, type in all_relationships:
+    #    print(f'{person1} has been a {type} of {person2} since {start_date}.')
+    
+    return spouse_relationships
 
 def save_married_couples_csv(married_couples, csv_path):
     """Saves list of married couples to a CSV file, including both people's 
@@ -39,6 +61,32 @@ def save_married_couples_csv(married_couples, csv_path):
     """
     # TODO: Function body
     # Hint: We did this in Lab 7.
+    
+    # Test who is in the list
+    #print(married_couples)
+    
+    # Create dataframe from married couples variable
+    df = pd.DataFrame(married_couples)
+
+    #Create csv file name
+    csv_filename = 'Report_of_married_couples.csv'
+    
+    #Create headings for csv file
+    headings = ('Person 1', 'Person 2', 'Anniversary')
+    
+    #Fix up path from csv_path
+    fixdirector = os.path.dirname(csv_path)
+    
+    #Join and create list_path
+    list_path = os.path.join(fixdirector, csv_filename)
+
+    # Test print path
+    #print(list_path)
+
+    df.to_csv(list_path, index=False, header=headings)
+
+    print(list_path)
+
     return
 
 if __name__ == '__main__':
